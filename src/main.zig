498b051a449793b09b69855fe95e7dad68724fbf
Kamil Konefke <kamilkonefke@proton.me>
2025-06-01 21:19:26 +0200

    feat: add map rendering and player movement

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,93 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
 
-var map: [][]u8 = undefined;
+var map: [8][8]u8 = undefined;
+
+const Player = struct {
+    x: i8,
+    y: i8
+};
+var player: Player = .{ .x = 1, .y = 1 };
+
+fn load_map() ![]const u8 {
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+
+    if (args.len != 2) {
+        return "";
+    }
+
+    return args[1];
+}
+
+fn generate_map() !void {
+    for(0..8) |y| {
+        for(0..8) |x| {
+            map[y][x] = 0;
+        }
+    }
+
+    map[5][5] = 2;
+    map[6][2] = 1;
+}
+
+fn render_map() !void {
+    for(0..8) |y| {
+        for(0..8) |x| {
+            var char = switch(map[y][x]) {
+                0 => ".", // Air
+                1 => "X", // Wall
+                2 => "#", // Box
+                else => "."
+            };
+
+            if (player.x == x and player.y == y) {
+                char = "@";
+            }
+
+            try stdout.print("{s}", .{ char });
+        }
+        try stdout.print("\n", .{});
+    }
+}
+
+fn render_statusbar() !void {
+    try stdout.print("Sokoban!\n", .{});
+}
+
+fn push_box(x: i8, y: i8, dx: i8, dy: i8) void {
+    const xx: usize = @intCast(x);
+    const yy: usize = @intCast(y);
+    const dxx: usize = @intCast(dx);
+    const dyy: usize = @intCast(dy);
+
+    if (map[yy][xx] == 2) {
+        map[yy][xx] = 0;
+        map[yy + dyy][xx + dxx] = 2;
+    }
+}
+
+fn is_cell_empty(x: i8, y: i8) bool {
+    const xx: usize = @intCast(x);
+    const yy: usize = @intCast(y);
+    if (map[yy][xx] == 1) {
+        return false;
+    } else {
+        return false;
+    }
+}
+
+fn player_move(dx: i8, dy: i8) {
+    if(is_cell_empty(dx, dy)) {
+
+    } else {
+
+    }
+}
 
 pub fn main() !void {
+    // const map_name = try load_map();
+
     // enter raw mode
     const tty_backup = try std.posix.tcgetattr(std.posix.STDIN_FILENO);
     defer std.posix.tcsetattr(std.posix.STDIN_FILENO, .FLUSH, tty_backup) catch {}; // restore tty
@@ -17,14 +101,38 @@ pub fn main() !void {
     tty_raw.lflag.ICANON = false;
     try std.posix.tcsetattr(std.posix.STDIN_FILENO, .FLUSH, tty_raw);
 
+    try generate_map();
+
     var input_buffer: [1]u8 = undefined;
     while (true) {
         // Rendering
         try stdout.writeAll("\x1B[2J\x1B[H");
+        try render_statusbar();
+        try render_map();
 
         // Input
         _ = try stdin.read(&input_buffer);
         switch (input_buffer[0]) {
+            'w' => {
+                if (is_cell_empty(player.x, player.y - 1)) {
+                    player.y -= 1;
+                }
+            },
+            's' => {
+                if (is_cell_empty(player.x, player.y + 1)) {
+                    player.y += 1;
+                }
+            },
+            'a' => {
+                if (is_cell_empty(player.x - 1, player.y)) {
+                    player.x -= 1;
+                }
+            },
+            'd' => {
+                if (is_cell_empty(player.x + 1, player.y)) {
+                    player.x += 1;
+                }
+            },
             'q' => return,
             else => {}
         }
